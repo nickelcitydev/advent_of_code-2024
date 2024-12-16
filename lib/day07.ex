dc5168955b8337f2a9212dbab3256b0b6306ebe1
Paul Theberge <paul@nickelcity.dev>
2024-12-16 17:28:13 -0500

    Day 7

--- lib/day07.ex
+++ lib/day07.ex
@@ -16,6 +16,10 @@ defmodule Day07 do
     multiply: &(&1 * &2)
   }
 
+  def concat(integer_1, integer_2) do
+    String.to_integer("#{integer_1}#{integer_2}")
+  end
+
   def part_one() do
     input = get_input()
 
@@ -35,7 +39,7 @@ defmodule Day07 do
       # Evaluate each operator sequence
       results =
         for ops_seq <- operator_sequences do
-          evaluate_expression(numbers, ops_seq)
+          evaluate_expression(numbers, ops_seq, @operators)
         end
 
       if solution in results do
@@ -46,10 +50,45 @@ defmodule Day07 do
     end)
   end
 
-  def evaluate_expression([num | nums], ops) do
+  def part_two() do
+    input = get_input()
+
+    # Just take the first tuple from the input as an example
+    Enum.reduce(input, 0, fn expression, acc ->
+      {solution, numbers} = expression
+
+      # We need length(numbers) - 1 operators for a single expression
+      num_op_slots = length(numbers) - 1
+
+      operators =
+        Map.put(@operators, :concat, fn left, right ->
+          String.to_integer("#{left}#{right}")
+        end)
+
+      # Extract the operator keys
+      operator_keys = Map.keys(operators)
+
+      # Generate all operator sequences of the required length
+      operator_sequences = cartesian_power(operator_keys, num_op_slots)
+
+      # Evaluate each operator sequence
+      results =
+        for ops_seq <- operator_sequences do
+          evaluate_expression(numbers, ops_seq, operators)
+        end
+
+      if solution in results do
+        acc + solution
+      else
+        acc
+      end
+    end)
+  end
+
+  def evaluate_expression([num | nums], ops, operators) do
     Enum.zip(nums, ops)
     |> Enum.reduce(num, fn {n, op}, acc ->
-      @operators[op].(acc, n)
+      operators[op].(acc, n)
     end)
   end
 
